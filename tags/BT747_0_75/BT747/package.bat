63dcaf9208c94512bb6e0a4e5f155b45479dd626
nobody <nobody@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2007-06-27 18:39:08 +0000

    This commit was manufactured by cvs2svn to create tag 'BT747_0_75'.

---
+++ tags/BT747_0_75/BT747/package.bat
@@ -0,0 +1,6 @@
+del BT747*.zip
+SET DT=20070626_235824
+
+zip -r BT747_%DT%.zip src dist license.txt README.txt -xi src/CVS/\* \*/CVS/\*
+rem curl -v -u anonymous:m.deweerd@ieee.org --upload-file BT747_%DT%.zip ftp://upload.sourceforge.net/incoming
+curl --ftp-pasv --ftp-skip-pasv-ip -u anonymous:m.deweerd@ieee.org -T BT747_%DT%.zip ftp://upload.sourceforge.net/incoming/BT747_%DT%.zip
