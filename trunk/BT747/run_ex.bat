e78be8d0cae5a1de4ce000fb72da3412b6c86909
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2007-07-05 22:19:25 +0000

    *** empty log message ***

--- trunk/BT747/run_ex.bat
+++ trunk/BT747/run_ex.bat
@@ -4,9 +4,8 @@ set ORGPATH=%PATH%
 REM Next line adds Java 1.4 installation path
 set JAVA_HOME=c:\j2sdk1.4.2_14
 set PATH=%JAVA_HOME%\bin;%PATH%
-set CLASSPATH=..\win32comm.jar;..\webstart\waba_only.jar;.;%CLASSPATH%
+set CLASSPATH=win32comm.jar;webstart\waba_only.jar;build\BT747.jar;.;%CLASSPATH%
 
-cd build
 java waba.applet.Applet BT747
 set PATH=%ORGPATH%
 pause
