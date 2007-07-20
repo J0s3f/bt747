17447642d59433a500d3d20efa9969dbbae23bfe
nobody <nobody@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2007-07-20 22:52:27 +0000

    This commit was manufactured by cvs2svn to create tag 'BT747_0_77_10'.

---
+++ tags/BT747_0_77_10/BT747/src/GPSconctrl.java
@@ -0,0 +1,99 @@
+//********************************************************************
+//***                           BT 747                             ***
+//***                      April 14, 2007                          ***
+//***                  (c)2007 Mario De Weerd                      ***
+//***                     m.deweerd@ieee.org                       ***
+//***  **********************************************************  ***
+//***  Software is provided "AS IS," without a warranty of any     ***
+//***  kind. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND WARRANTIES,***
+//***  INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS  ***
+//***  FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY    ***
+//***  EXCLUDED. THE ENTIRE RISK ARISING OUT OF USING THE SOFTWARE ***
+//***  IS ASSUMED BY THE USER. See the GNU General Public License  ***
+//***  for more details.                                           ***
+//***  *********************************************************** ***
+//***  The application was written using the SuperWaba toolset.    ***
+//***  This is a proprietary development environment based in      ***
+//***  part on the Waba development environment developed by       ***                                   
+//***  WabaSoft, Inc.                                              ***
+//********************************************************************                              
+import waba.ui.Button;
+import waba.ui.Container;
+import waba.ui.ControlEvent;
+import waba.ui.Event;
+import waba.ui.Label;
+import waba.ui.PushButtonGroup;
+
+import gps.GPSstate;
+
+/**
+ * @author Mario De Weerd
+ */
+
+public class GPSconctrl extends Container {
+    PushButtonGroup btnChannelSelect;
+
+    Button btnRestartGps;
+
+    GPSstate m_GPSstate;
+
+    static final int C_CHN_BLUETOOTH = 0;
+
+    static final int C_CHN_USB = 1;
+
+    static final int C_CHN_0 = 2;
+
+    static final String[] txtChannel = { "BLUETOOTH", "USB", "0", "1", "2",
+            "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14","15"};
+
+    public GPSconctrl(GPSstate p_GPSstate) {
+        m_GPSstate = p_GPSstate;
+    }
+
+    public void onStart() {
+
+        // Button group to select channel type
+        btnChannelSelect = new PushButtonGroup(txtChannel, // Names
+                true, // At least one
+                C_CHN_BLUETOOTH, // Default selected - get this from rxtx
+                -1, // gap
+                6, // inside gap
+                2, // rows
+                false, // all same width
+                PushButtonGroup.NORMAL);
+        add(btnChannelSelect, CENTER, AFTER + 2);
+        //gpsTimer = addTimer(100);
+
+        btnRestartGps = new Button("Reset COM port");
+        btnRestartGps.setGap(5);
+        add(btnRestartGps, RIGHT - 5, BOTTOM - 5);
+        add(new Label("Version:"+Version.NUMBER+"("+Version.DATE+")"), LEFT, BEFORE); //$NON-NLS-1$)
+    }
+
+    private void GPS_setChannel(int channel) {
+        switch (channel) {
+        case C_CHN_BLUETOOTH:
+            m_GPSstate.setBluetooth();
+            break;
+        case C_CHN_USB:
+            m_GPSstate.setUsb();
+            break;
+        default:
+            m_GPSstate.setPort(channel - C_CHN_0);
+            break;
+        }
+    }
+
+    public void onEvent(Event event) {
+        switch (event.type) {
+        case ControlEvent.PRESSED:
+            if (event.target == btnChannelSelect) {
+                GPS_setChannel(btnChannelSelect.getSelected());
+            } else if (event.target == btnRestartGps) {
+                m_GPSstate.GPS_restart();
+            }
+            break;
+        }
+    }
+
+}
---
+++ tags/BT747_0_77_10/BT747/src/gps/GPSPLTFile.java
@@ -0,0 +1,205 @@
+//********************************************************************
+//***                           BT 747                             ***
+//***                      April 14, 2007                          ***
+//***                  (c)2007 Mario De Weerd                      ***
+//***                     m.deweerd@ieee.org                       ***
+//***  **********************************************************  ***
+//***  Software is provided "AS IS," without a warranty of any     ***
+//***  kind. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND WARRANTIES,***
+//***  INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS  ***
+//***  FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY    ***
+//***  EXCLUDED. THE ENTIRE RISK ARISING OUT OF USING THE SOFTWARE ***
+//***  IS ASSUMED BY THE USER. See the GNU General Public License  ***
+//***  for more details.                                           ***
+//***  *********************************************************** ***
+//***  The application was written using the SuperWaba toolset.    ***
+//***  This is a proprietary development environment based in      ***
+//***  part on the Waba development environment developed by       ***                                   
+//***  WabaSoft, Inc.                                              ***
+//********************************************************************  
+package gps;
+
+import waba.io.File;
+import waba.sys.Convert;
+import waba.sys.Time;
+import waba.util.Date;
+
+/**Class to write a PLT file (OZI).
+ * @author Mario De Weerd
+ */
+public class GPSPLTFile implements GPSFile {
+    File m_File=null;
+    int m_recCount;
+    private GPSRecord activeFields;
+    
+    /**
+     * 
+     */
+    public GPSPLTFile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    GPSFilter m_Filter=null;
+    
+    public void setFilter(GPSFilter filter) {
+        m_Filter=filter;
+    }
+    
+    public boolean nextPass() {
+        return false;
+    }
+    /* (non-Javadoc)
+     * @see gps.GPSFile#InitialiseFile(java.lang.String, java.lang.String)
+     */
+    public void initialiseFile(final String basename, final String ext, final int Card) {
+        // TODO Auto-generated method stub
+        m_File=new File(basename+ext,File.DONT_OPEN,Card);
+        if(m_File.exists()) {
+            m_File.delete();
+        }
+        m_File=new File(basename+ext,File.CREATE, Card);
+        if(!m_File.isOpen()) {
+            waba.sys.Vm.debug("Could not open "+basename+ext);
+            m_File=null;
+        } else {
+            m_recCount=0;
+        }
+    }
+    
+    public void writeLogFmtHeader(final GPSRecord f) {
+        activeFields= new GPSRecord(f);
+        writeTxt("Datum, WGS 84\r\n");
+        //"NSAT (USED/VIEW),SAT INFO (SID-ELE-AZI-SNR)
+    }
+    
+    private final static int DAYS_BETWEEN_1970_1983=4748;
+    public Time utcTime(int utc_int) {
+        long utc=utc_int&0xFFFFFFFFL;
+        Time t=new Time();
+        t.second=(int)utc%60;
+        utc/=60;
+        t.minute=(int)utc%60;
+        utc/=60;
+        t.hour=(int)utc%24;
+        utc/=24;
+        // Now days since 1/1/1970
+        Date d= new Date(1,1,1983); //Minimum = 1983
+        d.advance(((int)utc)-DAYS_BETWEEN_1970_1983);
+        t.year=d.getYear();
+        t.month=d.getMonth();
+        t.day=d.getDay();
+        return t;
+    }
+    
+    private void writeTxt(final String s) {
+        m_File.writeBytes(s.getBytes(),0,s.length());
+    }
+    
+    // TODO: Should do something similar for  double 
+    private final String floatToString(final float f) {
+        String s="";
+        if(((f>=0.0)&&(f<1.0))||((f<0.0)&&(f>-1.0))) {
+            float myf = 0;
+            if (myf<0.0) {
+                myf=-f;
+                s="-";
+            } else {
+                myf=f;
+            }
+            int m=(int)(myf*1000.0+0.499999999999);
+            
+            if(m==0) {
+                s+="0.000";
+            } else if (m <10) {
+                s+="0.00";
+                s+=Convert.toString(m);
+            } else if (m <100) {
+                s+="0.0";
+                s+=Convert.toString(m);
+            } else {
+                s+="0."+Convert.toString(m);
+            }
+        } else {
+            s=Convert.toString(f,3);
+        }
+        return s;
+    }
+    
+    /* (non-Javadoc)
+     * @see gps.GPSFile#WriteRecord()
+     */
+    public void writeRecord(GPSRecord s) {
+        boolean prevField=false;
+        m_recCount++;
+        //rec+=Convert.toString(m_recCount);
+        
+        if(activeFields!=null && m_Filter.doFilter(s)) {
+            String rec="TP,UTM,56J,";
+
+            if(activeFields.latitude!=0) {
+                rec+=Convert.toString(s.latitude,6);
+            }
+            rec+=",";
+            if(activeFields.longitude!=0) {
+                rec+=Convert.toString(s.longitude,6);
+            }
+            rec+=",";
+            if(activeFields.utc!=0) {
+                Time t=utcTime(s.utc);
+                
+                rec+=
+                    ( t.month<10?"0":"")+Convert.toString(t.month)+"/"
+                    +(   t.day<10?"0":"")+Convert.toString(t.day)+"/"
+                    +Convert.toString(t.year)
+                    +","
+                    +(  t.hour<10?"0":"")+Convert.toString(t.hour)+":"
+                    +(t.minute<10?"0":"")+Convert.toString(t.minute)+":"
+                    +(t.second<10?"0":"")+Convert.toString(t.second)
+                    ;
+                if(activeFields.milisecond!=0) {
+                    rec+=".";
+                    rec+=(s.milisecond<100)?"0":"";
+                    rec+=(s.milisecond<10)?"0":"";
+                    rec+=Convert.toString(s.milisecond);
+                }
+            }
+            rec+=",";
+            if(activeFields.height!=0) {
+                rec+=floatToString(s.height);
+            }
+            rec+=",0,"; // Track section
+            if(activeFields.distance!=0) {
+                rec+=Convert.toString(s.distance); 
+            }
+            rec+=",";
+            //Time
+            rec+=",";
+            if(activeFields.speed!=0) {
+                rec+=floatToString(s.speed);
+            }
+            rec+=",";
+
+            if(activeFields.heading!=0) {
+                rec+=Convert.toString(s.heading);
+            }
+//            rec+=",";
+
+            rec+="\r\n";
+            writeTxt(rec);
+        } // activeFields!=null
+    }
+    
+    /* (non-Javadoc)
+     * @see gps.GPSFile#FinaliseFile()
+     */
+    public void finaliseFile() {
+        // TODO Auto-generated method stub
+        if(m_File!=null) {
+            m_File.close();
+            m_File=null;
+        }
+        
+    }
+    
+}
