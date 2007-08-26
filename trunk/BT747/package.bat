85db683fd0e0fc31ccf58efdfcab2603c9d5dc7b
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2007-08-26 11:28:39 +0000

    *** empty log message ***

--- trunk/BT747/package.bat
+++ trunk/BT747/package.bat
@@ -1,5 +1,5 @@
 del BT747*.zip
-SET DT=20070825_224931
+SET DT=20070826_132321
 
 SET EXTRA_FILES=run_ex.bat run_linux.sh webstart/Waba_only.jar win32comm.jar win32/javax.comm.properties win32com.dll Linux/BT747.jar Linux/javax.comm.properties Linux/swserial.properties Linux/librxtxSerial.so Linux/librxtxParallel.so  webstart/comm.jar webstart/RXTXcomm.jar build.xml .project .classpath
 
