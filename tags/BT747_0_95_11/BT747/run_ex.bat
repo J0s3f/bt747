1dac859eb2b1d50479d061f51a288441ddec4a4b
nobody <nobody@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2007-09-11 23:07:51 +0000

    This commit was manufactured by cvs2svn to create tag 'BT747_0_95_11'.

---
+++ tags/BT747_0_95_11/BT747/run_ex.bat
@@ -0,0 +1,11 @@
+
+set ORGPATH=%PATH%
+
+REM Next line adds Java 1.4 installation path
+set JAVA_HOME=c:\j2sdk1.4.2_14
+set PATH=%JAVA_HOME%\bin;%PATH%
+set CLASSPATH=win32comm.jar;webstart\waba_only.jar;dist\BT747.jar;.;%CLASSPATH%
+
+java waba.applet.Applet BT747
+set PATH=%ORGPATH%
+pause
