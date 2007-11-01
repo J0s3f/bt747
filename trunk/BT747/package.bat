0270fa74f3ea71f2ab1801ff04f26a8605e5c584
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2007-11-01 09:08:51 +0000

    Some changes for collaborative work.

--- trunk/BT747/package.bat
+++ trunk/BT747/package.bat
@@ -1,5 +1,5 @@
 del BT747*.zip
-SET DT=1.05
+SET DT=1.07
 
 SET FILES=ChangeLog.txt src dist license.txt README.txt  build.xml .project .classpath default.properties
 SET FILES=%FILES% run_ex.bat run_rxtx.bat run_rxtx.sh
