46cf6c05116bbe611a84896144635e3bd1e12478
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2007-11-12 00:33:32 +0000

    *** empty log message ***

---
+++ BT747/src_forwaba/bt747/sys/Convert.java
@@ -0,0 +1,30 @@
+/*
+ * Created on 12 nov. 2007
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package bt747.sys;
+
+/**
+ * @author Mario De Weerd
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public final class Convert {
+    public static String toString(boolean p)
+    {
+       return waba.sys.Convert.toString(p);
+    }
+
+    public static String toString(int p)
+    {
+       return waba.sys.Convert.toString(p);
+    }
+
+    public static String toString(float p)
+    {
+       return waba.sys.Convert.toString(p);
+    }
+}
