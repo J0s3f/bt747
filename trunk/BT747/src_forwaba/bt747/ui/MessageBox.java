f4b0dde94792237d2fd5748186e860b189224954
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2007-11-14 20:59:37 +0000

    Usage of the Waba environment moved to separate package to allow move to J2ME.

---
+++ trunk/BT747/src_forwaba/bt747/ui/MessageBox.java
@@ -0,0 +1,49 @@
+/*
+ * Created on 14 nov. 2007
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package bt747.ui;
+
+/**
+ * @author Mario De Weerd
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public final class MessageBox extends waba.ui.MessageBox {
+
+    /**
+     * @param title
+     * @param msg
+     */
+    public MessageBox(String title, String msg) {
+        super(title, msg);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param title
+     * @param text
+     * @param buttonCaptions
+     */
+    public MessageBox(String title, String text, String[] buttonCaptions) {
+        super(title, text, buttonCaptions);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param title
+     * @param text
+     * @param buttonCaptions
+     * @param gap
+     * @param insideGap
+     */
+    public MessageBox(String title, String text, String[] buttonCaptions,
+            int gap, int insideGap) {
+        super(title, text, buttonCaptions, gap, insideGap);
+        // TODO Auto-generated constructor stub
+    }
+
+}
