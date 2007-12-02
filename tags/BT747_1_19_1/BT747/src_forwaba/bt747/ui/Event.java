feeed0eeb624fadfd628e8da3955ef7878d8a3e0
nobody <nobody@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2007-12-02 00:18:36 +0000

    This commit was manufactured by cvs2svn to create tag 'BT747_1_19_1'.

---
+++ tags/BT747_1_19_1/BT747/src_forwaba/bt747/ui/Event.java
@@ -0,0 +1,35 @@
+/*
+ * Created on 14 nov. 2007
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package bt747.ui;
+
+/**
+ * @author Mario De Weerd
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Event extends waba.ui.Event {
+
+    /**
+     * 
+     */
+    public Event() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param type
+     * @param target
+     * @param timeStamp
+     */
+    public Event(int type, Object target, int timeStamp) {
+        super(type, target, timeStamp);
+        // TODO Auto-generated constructor stub
+    }
+
+}
