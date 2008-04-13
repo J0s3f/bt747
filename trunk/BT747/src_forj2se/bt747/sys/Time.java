89b1c92a7d3fc4f41c512fe997a52cbc64067efa
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2008-04-13 09:52:31 +0000

    Fixe date/time handling (was dependent on java implementation)

--- trunk/BT747/src_forj2se/bt747/sys/Time.java
+++ trunk/BT747/src_forj2se/bt747/sys/Time.java
@@ -6,6 +6,10 @@
  */
 package bt747.sys;
 
+import java.util.GregorianCalendar;
+
+import bt747.util.Date;
+
 
 
 /**
@@ -14,7 +18,7 @@ package bt747.sys;
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-public class Time extends java.util.Date {
+public class Time extends java.util.GregorianCalendar {
 
     /**
      * 
@@ -22,36 +26,65 @@ public class Time extends java.util.Date {
     private static final long serialVersionUID = -4377529854748199705L;
     
     public int getHour() {
-        return getHours();
+        return this.get(GregorianCalendar.HOUR_OF_DAY);
     }
     public void setHour(int hours) {
-        setHours(hours);
+        super.set(HOUR_OF_DAY, hours);
     }
     public int getMinute() {
-        return getMinutes();
+        return this.get(GregorianCalendar.MINUTE);
     }
     public void setMinute(int hours) {
-        setMinutes(hours);
+        super.set(MINUTE, hours);
     }
     public int getSecond() {
-        return getSeconds();
+        return this.get(GregorianCalendar.SECOND);
     }
     public void setSecond(int hours) {
-        setSeconds(hours);
+        super.set(SECOND, hours);
     }
     public void setDay(int date) {
-        setDate(date);
+        super.set(DAY_OF_MONTH, date);
     }
    
     public int getYear() {
-        return super.getYear()+1900;
+        return super.get(GregorianCalendar.YEAR);
     }
 
     public void setYear(int year) {
-        super.setYear(year-1900);
+        super.set(YEAR, year);
+    }
+    public int getMonth() {
+        return super.get(GregorianCalendar.MONTH);
+    }
+    public void setMonth(int month) {
+        super.set(MONTH, month);
+    }
+    public int getDay() {
+        return super.get(GregorianCalendar.DAY_OF_MONTH);
+    }
+    
+    
+    // UTC time in java depends on implementation (leap seconds, ...)
+    // So we need to implement our own function.
+    public final void setUTCTime(final int utc_int) {
+        // long utc=utc_int&0xFFFFFFFFL;
+        int utc = utc_int;
+        int hour;
+        int minute;
+        int second;
+        // Time t=new Time();
+        second=utc % 60;
+        utc /= 60;
+        minute=utc % 60;
+        utc /= 60;
+        hour=utc%24;
+        utc /= 24;
+        this.set(1970,1,1,hour,minute,second);
+        this.add(DAY_OF_YEAR, utc-1);
     }
 
-    public  final void setUTCTime(final int utc_int) {
-        setTime(utc_int*1000L);
-    }
+//    public  final void setUTCTime(final int utc_int) {
+//        super.setTime(new java.util.Date(utc_int*1000L));
+//    }
 }
