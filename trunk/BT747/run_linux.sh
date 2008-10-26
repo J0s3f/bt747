ba1e7f5d8af17a773492f91c955dc1e6288b9a2a
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2008-10-26 23:59:15 +0000

    Simplified release

--- trunk/BT747/run_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-CLASSPATH=lib/comm.jar:lib/RXTXcomm.jar:lib/Waba_only.jar:dist/BT747_rxtx.jar:.:$CLASSPATH
-export CLASSPATH
-
-#strace -e trace=file -f -o trace.log
-#java -Djava.library.path=${PWD}/Linux 
-java  waba.applet.Applet /w 320 /h 320 /scale 1 /bpp 8 BT747
