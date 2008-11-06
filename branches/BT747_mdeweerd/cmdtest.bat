0f6fbe164c06ae30b8ec60abe8c2c554d995c099
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2008-11-06 20:05:33 +0000

    Small local test script added. UPdate to BT747cmd related to initialisation.

--- branches/BT747_mdeweerd/cmdtest.bat
+++ branches/BT747_mdeweerd/cmdtest.bat
@@ -1,1 +1,2 @@
-./BT747cmd.bat -f c:/BT747/TST -b c:/BT747/in.csv --outtype CSV
+#./BT747cmd.bat -f c:/BT747/TST -b c:/BT747/in.csv --outtype CSV,NMEA
+./BT747cmd.bat -f n/tst -b c:/BT747/in.csv --outtype CSV,NMEA
