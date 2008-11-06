36a7f4ed4c110ef13155a3ed3979b175b901cfab
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2008-11-06 21:34:25 +0000

    NMEA improved

--- cmdtest.bat
+++ cmdtest.bat
@@ -1,2 +1,4 @@
 #./BT747cmd.bat -f c:/BT747/TST -b c:/BT747/in.csv --outtype CSV,NMEA
-./BT747cmd.bat -f n/tst -b c:/BT747/in.csv --outtype CSV,NMEA
+#./BT747cmd.bat -f n/tst -b c:/BT747/in.csv --outtype CSV,NMEA
+CALL ./BT747cmd.bat -f n/tst -b c:/BT747/in.csv --outtype CSV,NMEA
+CALL ./BT747cmd.bat -f n/tst2 -b n/tst.nmea --outtype CSV,NMEA
