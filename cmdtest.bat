adae4310c5987875dae3e92b7976f737b3c02c8b
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2008-11-11 21:34:24 +0000

    Update NMEA reading and writing + 64 bit jar for the web + jnlp for the web.

--- cmdtest.bat
+++ cmdtest.bat
@@ -5,4 +5,6 @@ CALL ./BT747cmd.bat --splittype DAY -f n/tst2 -b n/tst.nmea --outtype CSV,NMEA
 CALL ./BT747cmd.bat --splittype TRACK --timesplit 1 -f n/tst2 -b n/tst.nmea --outtype CSV,NMEA
 CALL ./BT747cmd.bat --splittype TRACK --timesplit 1 -f n/tst3 -b c:/BT747/in.csv --outtype CSV,NMEA --height
 CALL ./BT747cmd.bat --splittype TRACK --timesplit 1 -f n/tst4 -b c:/BT747/in.csv --outtype CSV,NMEA
+CALL ./BT747cmd.bat -f n/tst5 -b n/tst.nmea --outtype csv,nmea
 diff n/tst2-*1036.csv n/tst3-*1036.csv
+diff c:/BT747/in.csv n/tst5.csv
