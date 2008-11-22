f7f9a9deade2c2d0bc2f8cd190e18d96de5c0dd7
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2008-11-22 23:45:27 +0000

    Commit before packaging

--- BT_J2ME/src/net/sf/bt747/j4me/app/MTKMidlet.java
+++ BT_J2ME/src/net/sf/bt747/j4me/app/MTKMidlet.java
@@ -1,5 +1,15 @@
 package net.sf.bt747.j4me.app;
 
+import java.io.PrintStream;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.DateField;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.TextBox;
+import javax.microedition.lcdui.TextField;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
@@ -16,7 +26,7 @@ import bt747.sys.Interface;
 /**
  * The entry point for the application.
  */
-public class MTKMidlet extends MIDlet {
+public class MTKMidlet extends MIDlet implements CommandListener {
     /**
      * The one and only instance of this class.
      */
@@ -61,12 +71,15 @@ public class MTKMidlet extends MIDlet {
     protected void pauseApp() {
     }
 
+    private Command commandExit;
+
     /**
      * Called when the application starts. Shows the first screen.
      * 
      * @see javax.microedition.midlet.MIDlet#startApp()
      */
     protected final void startApp() throws MIDletStateChangeException {
+        Throwable m = null;
         // Initialize the J4ME UI manager.
         try {
             UIManager.init(this);
@@ -77,18 +90,64 @@ public class MTKMidlet extends MIDlet {
             // FindingGPSDevicesAlert creates a list of devices, then calls
             // SelectGPSScreen which will in turn call
             // InitializingGPSAlert
-            
 
             // (new ConvertTo(c, main)).doWork(); // Debug conversion
             main.show();
             // (new ConvertTo(c, main)).show();
         } catch (Throwable t) {
             Log.warn("Unhandled exception ", t);
+            m = t;
+        }
+        if(m!=null) {
             // LogScreen l = new LogScreen(null);
             // l.show();
+            Display disp;
+            Form form;
+
+            // fait un lien avec l'affichage
+            disp = Display.getDisplay(this);
+
+            // creation d'un objet formulaire sur lequel on peut placer des
+            // composants
+
+            form = new Form("BT747 " + getAppProperty("MIDlet-Version")
+                    + " Exception");
+
+            // creation d'un bouton pour sortir du programme
+            commandExit = new Command("Exit", Command.SCREEN, 1);
+
+            // Next lines for future reference (must be deleted here)
+            // javax.microedition.lcdui.DateField d;
+            // d = new DateField("help",DateField.DATE);
+            javax.microedition.lcdui.TextField tb;
+            tb = new TextField("", "", 250, TextField.ANY);
+            String s = m.getMessage();
+            String b = m.toString();
+            String f = "";
+            if (b != null) {
+                f += b;
+            }
+            if (s != null) {
+                if (f.length() > 0) {
+                    f += "\n";
+                }
+                f += s;
+            }
+            tb = new TextField(f, f, 250, TextField.ANY);
+
+            // creation d'un champ de texte contenant notre Hello World
+            // ajout des composants au formulaire
+            form.addCommand(commandExit);
+            form.append(tb);
+            // tb.setLabel(t.getMessage());
+            // tb.setString(t.getMessage());
+            form.setCommandListener(this);
+
+            // affichage du formulaire
+            disp.setCurrent(form);
         }
     }
-    
+
     /**
      * Called when the application is exiting.
      * 
@@ -112,6 +171,26 @@ public class MTKMidlet extends MIDlet {
             instance.destroyApp(true);
         } catch (MIDletStateChangeException e) {
             // Ignore.
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.microedition.lcdui.CommandListener#commandAction(javax.microedition.lcdui.Command,
+     *      javax.microedition.lcdui.Displayable)
+     */
+    public void commandAction(Command c, Displayable s) {
+        // lors du clic sur le bouton Exit
+        if (c == commandExit) {
+            // appel manuel à la fonction de fermeture
+            try {
+                destroyApp(false);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+            // on demande au manager de fermer l'application
+            notifyDestroyed();
         }
     }
 
