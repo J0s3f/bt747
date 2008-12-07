79af52ca0be3c07fa1965176f4a2711cf0be58eb
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2008-12-07 12:49:48 +0000

    Releasing working copy to trunk

---
+++ BT747/src/gps/log/GPSRecord.java
@@ -0,0 +1,340 @@
+//********************************************************************
+//***                           BT 747                             ***
+//***                      April 14, 2007                          ***
+//***                  (c)2007 Mario De Weerd                      ***
+//***                     m.deweerd@ieee.org                       ***
+//***  **********************************************************  ***
+//***  Software is provided "AS IS," without a warranty of any     ***
+//***  kind. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND WARRANTIES,***
+//***  INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS  ***
+//***  FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY    ***
+//***  EXCLUDED. THE ENTIRE RISK ARISING OUT OF USING THE SOFTWARE ***
+//***  IS ASSUMED BY THE USER.                                     ***
+//***  See the GNU General Public License Version 3 for details.   ***
+//***  *********************************************************** ***
+package gps.log;
+
+import bt747.sys.Convert;
+
+import gps.BT747Constants;
+import gps.log.out.CommonOut;
+
+/**
+ * Structure to hold GPS data for one point
+ * 
+ * @author Mario De Weerd
+ */
+public class GPSRecord {
+    public int utc;
+    public int valid;
+    public double latitude;
+    public double longitude;
+    public float height;
+    public float speed;
+    public float heading;
+    public int dsta;
+    public int dage;
+    public int pdop;
+    public int hdop;
+    public int vdop;
+    public int nsat;
+
+    public int[] sid;
+    public boolean[] sidinuse;
+    public int[] ele;
+    public int[] azi;
+    public int[] snr;
+
+    public int rcr;
+    /** Recorder reason */
+    public int milisecond;
+    public double distance;
+
+    public float geoid; // Value returned by GPS.
+    public int recCount;
+
+    public int logPeriod; // Programmed period between logged points
+    public int logSpeed; // Programmed minimum speed
+    public int logDistance; // Programmed distance between logged points
+
+    public String voxStr; // Voice recording reference.
+
+    public GPSRecord() {
+        utc = 0;
+    }
+
+    /**
+     * 
+     */
+    public GPSRecord(final GPSRecord r) {
+        this.utc = r.utc;
+        this.valid = r.valid;
+        this.latitude = r.latitude;
+        this.longitude = r.longitude;
+        this.height = r.height;
+        this.speed = r.speed;
+        this.heading = r.heading;
+        this.dsta = r.dsta;
+        this.dage = r.dage;
+        this.pdop = r.pdop;
+        this.hdop = r.hdop;
+        this.vdop = r.vdop;
+        this.nsat = r.nsat;
+        if (r.sid != null) {
+            // Object method clone() does not work on device for arrays.
+            // Doing explicit copy until better method found.
+            int i = r.sid.length;
+            this.sid = new int[i];
+            this.sidinuse = new boolean[i];
+            if (r.ele != null) {
+                this.ele = new int[i];
+            }
+            if (r.azi != null) {
+                this.azi = new int[i];
+            }
+            if (r.snr != null) {
+                this.snr = new int[i];
+            }
+
+            for (i -= 1; i >= 0; i--) {
+                this.sid[i] = r.sid[i];
+                this.sidinuse[i] = r.sidinuse[i];
+                if (r.ele != null) {
+                    this.ele[i] = r.ele[i];
+                }
+                if (r.azi != null) {
+                    this.azi[i] = r.azi[i];
+                }
+                if (r.snr != null) {
+                    this.snr[i] = r.snr[i];
+                }
+            }
+        }
+        this.rcr = r.rcr;
+        this.milisecond = r.milisecond;
+        this.distance = r.distance;
+        this.geoid = r.geoid;
+        this.recCount = r.recCount;
+        this.logDistance = r.logDistance;
+        this.logPeriod = r.logPeriod;
+        this.logSpeed = r.logSpeed;
+        if (r.voxStr != null) {
+            this.voxStr = r.voxStr;
+        }
+    }
+
+    public final GPSRecord cloneRecord() {
+        return new GPSRecord(this);
+    }
+
+    public final int getLogFormat() {
+        // Return log format based on content.
+        return 0xFFFFFFFF;
+    }
+
+    public final static GPSRecord getLogFormatRecord(final int logFormat) {
+        GPSRecord gpsRec = new GPSRecord();
+        if ((logFormat & (1 << BT747Constants.FMT_UTC_IDX)) == 0) {
+            gpsRec.utc = 0;
+        } else {
+            gpsRec.utc = 1;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_VALID_IDX)) == 0) {
+            gpsRec.valid = -1;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_LATITUDE_IDX)) == 0) {
+            gpsRec.latitude = -1000000;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_LONGITUDE_IDX)) == 0) {
+            gpsRec.longitude = -1000000;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_HEIGHT_IDX)) == 0) {
+            gpsRec.height = -1000000;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_SPEED_IDX)) == 0) {
+            gpsRec.speed = -1000000;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_HEADING_IDX)) == 0) {
+            gpsRec.heading = -1000000;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_DSTA_IDX)) == 0) {
+            gpsRec.dsta = -1;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_DAGE_IDX)) == 0) {
+            gpsRec.dage = -1;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_PDOP_IDX)) == 0) {
+            gpsRec.pdop = -1;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_HDOP_IDX)) == 0) {
+            gpsRec.hdop = -1;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_VDOP_IDX)) == 0) {
+            gpsRec.vdop = -1;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_NSAT_IDX)) == 0) {
+            gpsRec.nsat = -1;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_SID_IDX)) == 0) {
+            gpsRec.sid = null;
+            gpsRec.sidinuse = null;
+        } else {
+            gpsRec.sid = new int[0];
+            gpsRec.sidinuse = new boolean[0];
+        }
+
+        if ((logFormat & (1 << BT747Constants.FMT_ELEVATION_IDX)) == 0) {
+            gpsRec.ele = null;
+        } else {
+            gpsRec.ele = new int[0];
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_AZIMUTH_IDX)) == 0) {
+            gpsRec.azi = null;
+        } else {
+            gpsRec.azi = new int[0];
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_SNR_IDX)) == 0) {
+            gpsRec.snr = null;
+        } else {
+            gpsRec.snr = new int[0];
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_RCR_IDX)) == 0) {
+            gpsRec.rcr = -1;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_MILLISECOND_IDX)) == 0) {
+            gpsRec.milisecond = -1;
+        }
+        if ((logFormat & (1 << BT747Constants.FMT_DISTANCE_IDX)) == 0) {
+            gpsRec.distance = -1;
+        }
+
+        /* End handling record */
+        return gpsRec;
+    }
+
+    public final boolean hasUtc() {
+        return utc != 0;
+    }
+
+    public final boolean hasValid() {
+        return valid != -1;
+    }
+
+    public final boolean hasLatitude() {
+        return latitude != -1000000;
+    }
+
+    public final boolean hasLongitude() {
+        return longitude != -1000000;
+    }
+
+    public final boolean hasHeight() {
+        return height != -1000000;
+    }
+
+    public final boolean hasSpeed() {
+        return speed != -1000000;
+    }
+
+    public final boolean hasHeading() {
+        return heading != -1000000;
+    }
+
+    public final boolean hasDsta() {
+        return dsta != -1;
+    }
+
+    public final boolean hasDage() {
+        return dage != -1;
+    }
+
+    public final boolean hasPdop() {
+        return pdop != -1;
+    }
+
+    public final boolean hasHdop() {
+        return hdop != -1;
+    }
+
+    public final boolean hasVdop() {
+        return vdop != -1;
+    }
+
+    public final boolean hasNsat() {
+        return nsat != -1;
+    }
+
+    public final boolean hasSid() {
+        return sid != null;
+    }
+
+    public final boolean hasSidInUse() {
+        return sidinuse != null;
+    }
+
+    public final boolean hasEle() {
+        return ele != null;
+    }
+
+    public final boolean hasAzi() {
+        return azi != null;
+    }
+
+    public final boolean hasSnr() {
+        return snr != null;
+    }
+
+    public final boolean hasRcr() {
+        return nsat != -1;
+    }
+
+    public final boolean hasMilisecond() {
+        return milisecond != -1;
+    }
+
+    public final boolean hasDistance() {
+        return distance != -1;
+    }
+
+    public final boolean hasVoxStr() {
+        return voxStr != null;
+    }
+
+    public final boolean equalsFormat(final GPSRecord r) {
+        return (hasUtc() == r.hasUtc()) && (hasValid() == r.hasValid())
+                && (hasLatitude() == r.hasLatitude())
+                && (hasLongitude() == r.hasLongitude())
+                && (hasHeight() == r.hasHeight())
+                && (hasSpeed() == r.hasSpeed())
+                && (hasHeading() == r.hasHeading())
+                && (hasDsta() == r.hasDsta()) && (hasDage() == r.hasDage())
+                && (hasPdop() == r.hasPdop()) && (hasHdop() == r.hasHdop())
+                && (hasVdop() == r.hasVdop()) && (hasNsat() == r.hasNsat())
+                && (hasSid() == r.hasSid())
+                && (hasSidInUse() == r.hasSidInUse())
+                && (hasEle() == r.hasEle()) && (hasAzi() == r.hasAzi())
+                && (hasSnr() == r.hasSnr()) && (hasRcr() == r.hasRcr())
+                && (hasMilisecond() == r.hasMilisecond())
+                && (hasDistance() == r.hasDistance())
+                && (hasVoxStr() == r.hasVoxStr());
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        StringBuffer rec = new StringBuffer(100);
+        rec.setLength(0);
+        rec.append("CNT:" + recCount);
+        rec.append("\nUTC:" + gps.log.out.CommonOut.getTimeStr(utc) + "(" + utc
+                + ")");
+        rec.append("\nVALID:" + Convert.unsigned2hex(valid, 8));
+        rec.append("\nLAT;" + latitude);
+        rec.append("\nLON:" + longitude);
+        rec.append("\nRCR:" + Convert.unsigned2hex(rcr, 8));
+        rec.append('\n');
+        return rec.toString();
+    }
+}
---
+++ BT747/src_waba/bt747/waba_view/AppController.java
@@ -0,0 +1,392 @@
+package bt747.waba_view;
+
+import waba.sys.Settings;
+
+import net.sf.bt747.waba.system.WabaFile;
+import gps.BT747Constants;
+import gps.log.GPSRecord;
+import moio.util.HashSet;
+
+import bt747.Txt;
+import bt747.model.AppSettings;
+import bt747.model.BT747View;
+import bt747.model.Controller;
+import bt747.model.Model;
+
+import bt747.sys.File;
+import bt747.sys.Generic;
+import bt747.waba_view.ui.BT747MessageBox;
+// #if RXTX import bt747.sys.Convert;
+
+//import moio.util.Iterator;  Needed later when communicating with views.
+
+public final class AppController extends Controller {
+
+    private static String CONFIG_FILE_NAME =
+    // #if RXTX java.lang.System.getProperty("bt747_settings", // bt747_settings
+    // or default value
+    // #if RXTX ((java.lang.System.getProperty("user.home").length()!=0) ?
+    // #if RXTX java.lang.System.getProperty("user.home")
+    // #if RXTX +java.lang.System.getProperty("file.separator")
+    // #if RXTX +"SettingsBT747.pdb":(
+
+    (Settings.platform.startsWith("Win32")
+            || Settings.platform.startsWith("Windows") || Settings.platform
+            .startsWith("Mac")
+    // #if RXTX || java.lang.System.getProperty("os.name").startsWith("Mac")
+    ) ? "SettingsBT747.pdb" : "/My Documents/SettingsBT747.pdb"
+    // #if RXTX )
+    // #if RXTX ))
+    ;
+
+    /**
+     * The lower level controller. This should become a separate instance in the
+     * future.
+     */
+    private Controller c;
+
+    /**
+     * Reference to the model.
+     */
+    private Model m;
+
+    /**
+     * @param model
+     *            The model to associate with this controller.
+     */
+    public AppController(final Model model) {
+        this.m = model;
+        c = this; // Temporary solution until application controller methods
+
+        initAppSettings();
+        m.init();
+        // moved from lower level Controller.
+        super.setModel(m);
+        super.init();
+        // c = new Controller(model);
+
+        // Set up the (default) port handler
+    }
+
+    // The next methods are to be moved to the application controller.
+    /**
+     * Convert the log given the provided parameters using other methods.
+     * 
+     * @param logType
+     *            Indicates the type of log that should be written. For example
+     *            Model.CSV_LOGTYPE .
+     * @see Model#CSV_LOGTYPE
+     * @see Model#TRK_LOGTYPE
+     * @see Model#KML_LOGTYPE
+     * @see Model#PLT_LOGTYPE
+     * @see Model#GPX_LOGTYPE
+     * @see Model#NMEA_LOGTYPE
+     * @see Model#GMAP_LOGTYPE
+     */
+    public final void convertLog(final int logType) {
+        if (doConvertLog(logType) != 0) {
+            reportError(c.getLastError(), c.getLastErrorInfo());
+        }
+    }
+
+    /**
+     * Convert the log into an array of trackpoints.
+     * 
+     * @return Array of selected trackpoints.
+     */
+    public final GPSRecord[] convertLogToTrackPoints() {
+        GPSRecord[] result;
+        result = c.doConvertLogToTrackPoints();
+        if (result == null) {
+            reportError(c.getLastError(), c.getLastErrorInfo());
+        }
+        return result;
+    }
+
+    /** Options for the first warning message. */
+    private static final String[] C_ERASE_OR_CANCEL = { Txt.getString(Txt.ERASE), Txt.getString(Txt.CANCEL) };
+    /** Options for the first warning message. */
+    private static final String[] C_YES_OR_CANCEL = { Txt.getString(Txt.YES), Txt.getString(Txt.CANCEL) };
+    /** Options for the second warning message - reverse order on purpose. */
+    private static final String[] C_CANCEL_OR_CONFIRM_ERASE = { Txt.getString(Txt.CANCEL),
+            Txt.getString(Txt.CONFIRM_ERASE) };
+
+    /**
+     * A 'recovery Erase' attempts to recover memory that was previously
+     * identified as 'bad'.
+     */
+    public final void recoveryErase() {
+        /** Object to open multiple message boxes */
+        BT747MessageBox mb;
+        mb = new BT747MessageBox(Txt.getString(Txt.TITLE_ATTENTION), Txt.getString(Txt.C_msgEraseWarning),
+                C_ERASE_OR_CANCEL);
+        mb.popupBlockingModal();
+        if (mb.getPressedButtonIndex() == 0) {
+            mb = new BT747MessageBox(Txt.getString(Txt.TITLE_ATTENTION),
+                    Txt.getString(Txt.C_msgEraseWarning2), C_CANCEL_OR_CONFIRM_ERASE);
+            mb.popupBlockingModal();
+            if (mb.getPressedButtonIndex() == 1) {
+                // Erase log
+                c.recoveryEraseLog();
+            }
+        }
+    }
+
+    /**
+     * (User) request to change the log format. Warns about requirement to erase
+     * the log too.
+     * 
+     * @param logFormat
+     *            The logFormat to set upon erase.
+     */
+    public final void changeLogFormatAndErase(final int logFormat) {
+        /** Object to open multiple message boxes */
+        BT747MessageBox mb;
+        mb = new BT747MessageBox(Txt.getString(Txt.TITLE_ATTENTION),
+                Txt.getString(Txt.C_msgWarningFormatAndErase), C_ERASE_OR_CANCEL);
+        mb.popupBlockingModal();
+        if (mb.getPressedButtonIndex() == 0) {
+            mb = new BT747MessageBox(Txt.getString(Txt.TITLE_ATTENTION),
+                    Txt.getString(Txt.C_msgWarningFormatAndErase2), C_CANCEL_OR_CONFIRM_ERASE);
+            mb.popupBlockingModal();
+            if (mb.getPressedButtonIndex() == 1) {
+                // Set format and reset log
+                c.setLogFormat(logFormat);
+                c.eraseLog();
+            }
+        }
+    }
+
+    /**
+     * (User) request to change the log format. The log is not erased and may be
+     * incompatible with other applications.
+     * 
+     * @param logFormat
+     *            The new log format to set.
+     */
+    public final void changeLogFormat(final int logFormat) {
+        /** Object to open multiple message boxes */
+        BT747MessageBox mb;
+        mb = new BT747MessageBox(true, Txt.getString(Txt.TITLE_ATTENTION),
+                Txt.getString(Txt.C_msgWarningFormatIncompatibilityRisk), C_YES_OR_CANCEL);
+        mb.popupBlockingModal();
+        if (mb.getPressedButtonIndex() == 0) {
+            c.setLogFormat(logFormat);
+        }
+    }
+
+    /**
+     * User request to format the device. Warns about requirement to erase
+     * the log too.
+     */
+    public final void eraseLogWithDialogs() {
+        /** Object to open multiple message boxes */
+        BT747MessageBox mb;
+        mb = new BT747MessageBox(Txt.getString(Txt.TITLE_ATTENTION), Txt.getString(Txt.C_msgEraseWarning),
+                C_ERASE_OR_CANCEL);
+        mb.popupBlockingModal();
+        if (mb.getPressedButtonIndex() == 0) {
+            mb = new BT747MessageBox(Txt.getString(Txt.TITLE_ATTENTION),
+                    Txt.getString(Txt.C_msgEraseWarning2), C_CANCEL_OR_CONFIRM_ERASE);
+            mb.popupBlockingModal();
+            if (mb.getPressedButtonIndex() == 1) {
+                // Erase log
+                c.eraseLog();
+            }
+        }
+    }
+
+    /**
+     * Report an error.
+     * 
+     * @param error
+     *            The error number.
+     * @param errorInfo
+     *            A text string related to the error (filename, ...).
+     */
+    private void reportError(final int error, final String errorInfo) {
+        String errorMsg;
+        switch (error) {
+        case BT747Constants.ERROR_COULD_NOT_OPEN:
+            errorMsg = Txt.getString(Txt.COULD_NOT_OPEN) + errorInfo;
+            Generic.debug(errorMsg, null);
+            new BT747MessageBox(Txt.getString(Txt.ERROR), errorMsg).popupBlockingModal();
+            break;
+        case BT747Constants.ERROR_NO_FILES_WERE_CREATED:
+            (new BT747MessageBox(Txt.getString(Txt.WARNING), Txt.getString(Txt.NO_FILES_WERE_CREATED)))
+                    .popupBlockingModal();
+            break;
+        case BT747Constants.ERROR_READING_FILE:
+            new BT747MessageBox(Txt.getString(Txt.ERROR), Txt.getString(Txt.PROBLEM_READING) + errorInfo)
+                    .popupBlockingModal();
+            break;
+        default:
+            break;
+        }
+    }
+
+    /**
+     * The list of views attached to this controller.
+     */
+    private HashSet views = new HashSet();
+
+    /**
+     * Attach a view to the controller.
+     * 
+     * @param view
+     *            The view that must be attached.
+     */
+    public final void addView(final BT747View view) {
+        views.add(view);
+        view.setController(this);
+        view.setModel(this.m);
+    }
+
+    /*
+     * Overriding the operations to be performed after successfull connect.
+     * 
+     * @see bt747.model.Controller#performOperationsAfterGPSConnect()
+     */
+    protected final void performOperationsAfterGPSConnect() {
+        if (m.isConnected()) {
+            if (m.getBooleanOpt(AppSettings.IS_STOP_LOGGING_ON_CONNECT)) {
+                c.setLoggingActive(false); // First command could fail, so repeat.
+                c.setLoggingActive(false);
+            }
+            super.performOperationsAfterGPSConnect();
+            saveSettings();
+        }
+    }
+
+    // protected void postEvent(final int type) {
+    // Iterator it = views.iterator();
+    // while (it.hasNext()) {
+    // BT747View l=(BT747View)it.next();
+    // Event e=new Event(l, type, null);
+    // l.newEvent(e);
+    // }
+    // }
+    private boolean isWin32LikeDevice() {
+        return waba.sys.Settings.platform.startsWith("WindowsCE")
+                || waba.sys.Settings.platform.startsWith("PocketPC")
+                || (waba.sys.Settings.platform.startsWith("Win32") && Settings.onDevice);
+    }
+
+    private void initAppSettings() {
+        AppSettings.defaultChunkSize = waba.sys.Settings.onDevice ? 220
+                : 0x10000;
+        AppSettings.defaultTraversable = waba.sys.Settings.onDevice
+                && (!waba.sys.Settings.platform.startsWith("Palm"));
+
+        if (waba.sys.Settings.platform.startsWith("Palm")) {
+            AppSettings.defaultBaseDirPath = "/Palm";
+        } else if (isWin32LikeDevice()) {
+            if (WabaFile.getCardVolumePath() == null) {
+                AppSettings.defaultBaseDirPath = "/EnterYourDir";
+            } else {
+                AppSettings.defaultBaseDirPath = WabaFile.getCardVolumePath();
+
+            }
+        } else {
+            AppSettings.defaultBaseDirPath = "/BT747";
+        }
+
+        if ((bt747.sys.Settings.getAppSettings() == null)
+                || (bt747.sys.Settings.getAppSettings().length() < 100)
+        // #if RXTX ||java.lang.System.getProperty("bt747_settings")!=null
+        ) {
+            bt747.sys.Settings.setAppSettings(new String(new byte[2048]));
+
+            if (isWin32LikeDevice()
+            // #if RXTX ||java.lang.System.getProperty("os.name")
+            // #if RXTX .startsWith("Mac")
+            // #if RXTX ||java.lang.System.getProperty("bt747_settings")!=null
+            // #if RXTX ||java.lang.System.getProperty("user.home").length()!=0
+            ) {
+                int readLength = 0;
+
+                // bt747.sys.Vm.debug("on Device "+bt747.sys.Settings.platform);
+                // bt747.sys.Vm.debug("loading config file "+CONFIG_FILE_NAME);
+                File preferencesFile = new File("");
+                try {
+                    preferencesFile = new File(CONFIG_FILE_NAME, File.READ_ONLY);
+                    readLength = preferencesFile.getSize();
+                    if (readLength >= 100) {
+                        byte[] appSettingsArray = new byte[2048];
+
+                        preferencesFile.readBytes(appSettingsArray, 0,
+                                readLength);
+                        bt747.sys.Settings.setAppSettings(new String(
+                                appSettingsArray));
+                    }
+                } catch (Exception e) {
+                    // Vm.debug("Exception new log create");
+                }
+                try {
+                    preferencesFile.close();
+                } catch (Exception e) {
+
+                }
+            }
+        }
+
+        // #if RXTX if(Convert.toInt(
+        // #if RXTX java.lang.System.getProperty("bt747_Mac_solvelag",
+        // #if RXTX java.lang.System.getProperty("os.name")
+        // #if RXTX .startsWith("Mac")?"1":"0"))==1) {
+        // #if RXTX AppSettings.setSolveMacLagProblem(true);
+        // #if RXTX }
+    }
+
+    /* @author Herbert Geus (initial code for saving settings on WindowsCE) */
+
+    public final void saveSettings() {
+        if (Generic.isDebug()) {
+            Generic.debug("Platform " + Settings.platform, null);
+            // #if RXTX Generic.debug( "os.name:" +
+            // #if RXTX java.lang.System.getProperty("os.name"), null);
+            // #if RXTX Generic.debug( "bt747_settings:" +
+            // #if RXTX java.lang.System.getProperty("bt747_settings"), null);
+            Generic.debug("Saving config file " + CONFIG_FILE_NAME, null);
+            Generic.debug("If true: " + isWin32LikeDevice(), null);
+        }
+        if (isWin32LikeDevice()
+        // #if RXTX ||java.lang.System.getProperty("os.name").startsWith("Mac")
+        // #if RXTX ||java.lang.System.getProperty("bt747_settings")!=null
+        ) {
+            File preferencesFile = new File("");
+            try {
+                File dir = new File(CONFIG_FILE_NAME.substring(0,
+                        CONFIG_FILE_NAME.lastIndexOf('/')), File.DONT_OPEN);
+                if (!dir.exists()) {
+                    dir.createDir();
+                }
+            } catch (Exception e) {
+                // Vm.debug("Exception new log delete");
+                // e.printStackTrace();
+            }
+            try {
+                preferencesFile = new File(CONFIG_FILE_NAME, File.DONT_OPEN);
+                if (preferencesFile.exists()) {
+                    preferencesFile.delete();
+                }
+            } catch (Exception e) {
+                Generic.debug("Exception config delete", e);
+            }
+            try {
+                preferencesFile = new File(CONFIG_FILE_NAME, File.CREATE);
+                preferencesFile.close();
+                preferencesFile = new File(CONFIG_FILE_NAME, File.READ_WRITE);
+                preferencesFile.writeBytes(bt747.sys.Settings.getAppSettings()
+                        .getBytes(), 0, bt747.sys.Settings.getAppSettings()
+                        .length());
+                preferencesFile.close();
+            } catch (Exception e) {
+                Generic.debug("Exception config create", e);
+            }
+            // bt747.sys.Vm.debug("saved config file length
+            // "+Settings.appSettings.length());
+        }
+    }
+
+}
