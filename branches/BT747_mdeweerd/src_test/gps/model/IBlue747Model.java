25afc49bc966bbf1d874f6b84374ee92921e9d9f
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2008-12-21 21:37:07 +0000

    Fixes in CSV output format and GPSRecord RCR field testing. Updated IBlue747 model for some testing.

--- branches/BT747_mdeweerd/src_test/gps/model/IBlue747Model.java
+++ branches/BT747_mdeweerd/src_test/gps/model/IBlue747Model.java
@@ -12,17 +12,19 @@
 //***  IS ASSUMED BY THE USER. See the GNU General Public License  ***
 //***  for more details.                                           ***
 //***  *********************************************************** ***
-//***  The application was written using the SuperWaba toolset.    ***
-//***  This is a proprietary development environment based in      ***
-//***  part on the Waba development environment developed by       ***                                   
-//***  WabaSoft, Inc.                                              ***
-//********************************************************************                              
 package gps.model;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import bt747.j2se_view.BT747cmd;
+import bt747.model.Controller;
+import bt747.model.Model;
 import bt747.sys.Convert;
-import waba.ui.Control;
-import waba.ui.ControlEvent;
-import waba.ui.Event;
+import bt747.sys.Generic;
+import bt747.sys.Interface;
 
 import gps.BT747Constants;
 import gps.connection.GPSrxtx;
@@ -33,11 +35,41 @@ import gps.convert.Conv;
  * 
  * @author Mario De Weerd
  */
-public class IBlue747Model extends Control {
+public class IBlue747Model {
 
     public int logFormat = 0x3E;
 
-    GPSrxtx m_GPSrxtx = null;
+    static GPSrxtx m_GPSrxtx = null;
+
+    /**
+     * Set up system specific classes.
+     */
+    static {
+        // Set up the low level functions interface.
+        Interface
+                .setJavaTranslationInterface(new net.sf.bt747.j2se.system.J2SEJavaTranslations());
+        // Set the serial port class instance to use (also system specific).
+        GPSrxtx.setGpsPortInstance(new gps.connection.GPSRxTxPort());
+
+    }
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            // Model m = new Model();
+            // Controller c = new Controller(m);
+
+            public void run() {
+                (new IBlue747Model()).onStart();
+            }
+        });
+
+        m_GPSrxtx = new GPSrxtx();
+        m_GPSrxtx.setDefaults(20, 115200);
+
+        Generic.setDebugLevel(1);
+    }
 
     /**
      * 
@@ -47,19 +79,57 @@ public class IBlue747Model extends Control {
 
     public void onStart() {
         m_GPSrxtx = new GPSrxtx();
-        m_GPSrxtx.setDefaults(2, 115200);
-        // m_GPSrxtx.openPort();
-        addTimer(10); // Palm minimum timer resolution= 10 ms
+        m_GPSrxtx.setDefaults(20, 115200);
+        m_GPSrxtx.openPort();
+        // addTimer(10); // Palm minimum timer resolution= 10 ms
+
+        TimerTask t;
+        t = new TimerTask() {
+
+            @Override
+            public void run() {
+                try {
+                    if (m_GPSrxtx.isConnected()) {
+                        lastResponse = m_GPSrxtx.getResponse();
+                        if (lastResponse != null) {
+                            analyseNMEA(lastResponse);
+                        }
+                    } else {
+                        m_GPSrxtx.setFreeTextPortAndOpen("COM20");
+                    }
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
+            }
+        };
+        Timer tm = new Timer();
+        tm.scheduleAtFixedRate(t, 100, 100);
     }
 
     public void replyMTK_Ack(final String[] p_nmea) {
+        try {
+        m_GPSrxtx.sendPacket("PMTK" + BT747Constants.PMTK_ACK_STR + ","
+                + p_nmea[0].substring(4) + ","
+                + BT747Constants.PMTK_ACK_SUCCEEDED);
+        } catch (Exception e) {
+            Generic.debug("Send failed ",e);
+        }
+    }
+
+    
+    public void replyMTK_Log_Ack(final String[] p_nmea) {
+        try {
         m_GPSrxtx.sendPacket("PMTK" + BT747Constants.PMTK_ACK_STR + ","
                 + BT747Constants.PMTK_CMD_LOG + "," + p_nmea[1] + ","
                 + BT747Constants.PMTK_ACK_SUCCEEDED);
+        } catch (Exception e) {
+            Generic.debug("Send failed ",e);
+        }
     }
 
     public int replyLogNmea(final String[] p_nmea) {
-        if (p_nmea.length > 2) {
+        try {
+            replyMTK_Log_Ack(p_nmea);
             switch (Convert.toInt(p_nmea[1])) {
             case BT747Constants.PMTK_LOG_Q:
                 // Parameter information
@@ -88,7 +158,7 @@ public class IBlue747Model extends Control {
                     case BT747Constants.PMTK_LOG_REC_METHOD: // 6;
                         break;
                     case BT747Constants.PMTK_LOG_LOG_STATUS: // 7; // bit 2 =
-                                                                // logging
+                        // logging
                         // on/off
                         break;
                     case BT747Constants.PMTK_LOG_MEM_USED: // 8;
@@ -119,6 +189,8 @@ public class IBlue747Model extends Control {
             default:
                 // Nothing - unexpected
             }
+        } catch (Exception e) {
+            // TODO: handle exception
         }
         return 0; // Done.
 
@@ -129,11 +201,22 @@ public class IBlue747Model extends Control {
         int z_Result = 0;
         // if(GPS_DEBUG) {
         // waba.sys.Vm.debug("ANA:"+p_nmea[0]+","+p_nmea[1]+"\n");}
+        StringBuffer nmea = new StringBuffer();
+        for (String s : p_nmea) {
+            nmea.append(s);
+            nmea.append(',');
+        }
+
+        Generic.debug(nmea.toString());
+
         if (p_nmea[0].startsWith("PMTK")) {
-            replyMTK_Ack(p_nmea);
             z_Cmd = Convert.toInt(p_nmea[0].substring(4));
 
-            z_Result = -1; // Suppose cmd not treated
+            if(z_Cmd!=BT747Constants.PMTK_CMD_LOG) {
+              replyMTK_Ack(p_nmea);
+            }
+
+            z_Result = -1; // Suppose cmd not treated            
             switch (z_Cmd) {
             case BT747Constants.PMTK_CMD_LOG: // CMD 182;
                 z_Result = replyLogNmea(p_nmea);
@@ -164,16 +247,19 @@ public class IBlue747Model extends Control {
             case BT747Constants.PMTK_API_Q_DATUM: // CMD 430
             case BT747Constants.PMTK_API_Q_DATUM_ADVANCE: // CMD 431
             case BT747Constants.PMTK_API_Q_GET_USER_OPTION: // CMD 490
-            // case BT747_dev.PMTK_DT_FIX_CTL: // CMD 500
-            // case BT747_dev.PMTK_DT_DGPS_MODE: // CMD 501
-            // case BT747_dev.PMTK_DT_SBAS: // CMD 513
-            // case BT747_dev.PMTK_DT_NMEA_OUTPUT: // CMD 514
-            // case BT747_dev.PMTK_DT_PWR_SAV_MODE: // CMD 520
-            // case BT747_dev.PMTK_DT_DATUM: // CMD 530
-            // case BT747_dev.PMTK_DT_FLASH_USER_OPTION: // CMD 590
-            case BT747Constants.PMTK_Q_VERSION: // CMD 604
-            default:
-                // Not handled
+                // case BT747_dev.PMTK_DT_FIX_CTL: // CMD 500
+                // case BT747_dev.PMTK_DT_DGPS_MODE: // CMD 501
+                // case BT747_dev.PMTK_DT_SBAS: // CMD 513
+                // case BT747_dev.PMTK_DT_NMEA_OUTPUT: // CMD 514
+                // case BT747_dev.PMTK_DT_PWR_SAV_MODE: // CMD 520
+                // case BT747_dev.PMTK_DT_DATUM: // CMD 530
+                // case BT747_dev.PMTK_DT_FLASH_USER_OPTION: // CMD 590
+            case BT747Constants.PMTK_Q_RELEASE:
+                m_GPSrxtx.sendPacket("PMTK" + BT747Constants.PMTK_DT_RELEASE
+                        + "," + "AXN_1.0-B_1.3_C01" + "," + "0001" + ","
+                        + "TSI_747A+" + "," + "1.0");
+                break;
+            case BT747Constants.PMTK_Q_VERSION:
                 break;
             } // End switch
         } // End if
@@ -182,18 +268,19 @@ public class IBlue747Model extends Control {
 
     public String[] lastResponse;
 
-    public void onEvent(Event e) {
-        switch (e.type) {
-        case ControlEvent.TIMER:
-            if (m_GPSrxtx.isConnected()) {
-                lastResponse = m_GPSrxtx.getResponse();
-                if (lastResponse != null) {
-                    analyseNMEA(lastResponse);
-                }
-            } else {
-            }
-            break;
+    // public void onEvent(Event e) {
+    // switch (e.type) {
+    // case ControlEvent.TIMER:
+    // if (m_GPSrxtx.isConnected()) {
+    // lastResponse = m_GPSrxtx.getResponse();
+    // if (lastResponse != null) {
+    // analyseNMEA(lastResponse);
+    // }
+    // } else {
+    // }
+    // break;
+    //
+    // }
+    // }
 
-        }
-    }
 }
