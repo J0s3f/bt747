ffff7e65bf04a1117b3ea50210ed99c3b285119a
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2009-05-03 12:20:10 +0000

    Not handling images without a position when writing back positions.

--- BT747_1.X/src_j2se/bt747/j2se_view/model/ImageData.java
+++ BT747_1.X/src_j2se/bt747/j2se_view/model/ImageData.java
@@ -21,7 +21,6 @@ import net.sf.bt747.j2se.app.exif.ExifAttribute;
 import net.sf.bt747.j2se.app.exif.ExifConstants;
 import net.sf.bt747.j2se.app.exif.ExifJPG;
 
-import bt747.sys.JavaLibBridge;
 import bt747.sys.File;
 import bt747.sys.JavaLibBridge;
 import bt747.sys.interfaces.BT747Date;
@@ -203,10 +202,11 @@ public class ImageData extends FileWaypoint {
                     day = JavaLibBridge.toInt(DateTime.substring(8, 10));
                     seconds = JavaLibBridge.toInt(DateTime.substring(11, 13))
                             * 3600
-                            + JavaLibBridge.toInt(DateTime.substring(14, 16)) * 60
+                            + JavaLibBridge.toInt(DateTime.substring(14, 16))
+                            * 60
                             + JavaLibBridge.toInt(DateTime.substring(17, 19));
-                    final BT747Date d = JavaLibBridge.getDateInstance(day, month,
-                            year);
+                    final BT747Date d = JavaLibBridge.getDateInstance(day,
+                            month, year);
                     setUtc(d.dateToUTCepoch1970() + seconds);
                 }
             } else {
@@ -229,7 +229,7 @@ public class ImageData extends FileWaypoint {
             final int card) {
         final ExifJPG exifJpg = new ExifJPG();
         exifJpg.setPath(orgPath); // Get exif data from file
-        if (getGpsRecord().hasLatitude() && getGpsRecord().hasLongitude()) {
+        if (getGpsRecord().hasPosition()) {
             final GPSRecord g = getGpsRecord();
             if (g.hasPosition()) {
                 exifJpg.setGpsPosition(g.latitude, g.longitude);
@@ -262,7 +262,7 @@ public class ImageData extends FileWaypoint {
                 if (satInfo.length() != 0) {
                     satInfo += " ";
                 }
-                satInfo += nsatInfoToString(g);
+                satInfo += ImageData.nsatInfoToString(g);
             }
             if (satInfo.length() != 0) {
                 exifJpg.setGpsSatInformation(satInfo);
