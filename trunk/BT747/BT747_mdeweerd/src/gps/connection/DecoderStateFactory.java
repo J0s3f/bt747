3b386dcc4ec4414823e8bba1c14d0b3218886555
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2009-05-05 00:11:54 +0000

    Releasing working copy to trunk

---
+++ trunk/BT747/BT747_mdeweerd/src/gps/connection/DecoderStateFactory.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package gps.connection;
+
+import bt747.sys.Generic;
+import bt747.sys.JavaLibBridge;
+import bt747.sys.interfaces.BT747Hashtable;
+
+/**
+ * @author Mario
+ * 
+ */
+public final class DecoderStateFactory {
+    private final static BT747Hashtable states = JavaLibBridge
+            .getHashtableInstance(2);
+
+    public final static int NMEA_STATE = 0;
+    public final static int DPL700_STATE = 1;
+    public final static int SIRFIII_STATE = 2;
+    public final static int MTKBIN_STATE = 3;
+
+    /**
+     * Gets an instance for the requested state. Currently a Singleton
+     * instance.
+     * 
+     * @param stateClass
+     * @return Instance for the state.
+     */
+    public final static DecoderStateInterface getInstance(final int state) {
+        final String stateStr = "" + state;
+        final DecoderStateInterface instanceFromHash = (DecoderStateInterface) states
+                .get("" + stateStr);
+        if (instanceFromHash != null) {
+            return instanceFromHash;
+        } else {
+            DecoderStateInterface newState;
+            switch (state) {
+            default:
+            case NMEA_STATE:
+                newState = new NMEADecoderState();
+                break;
+            case DPL700_STATE:
+                newState = new DPL700DecoderState();
+                break;
+            case SIRFIII_STATE:
+                newState = new SirfDecoderState();
+                break;
+            case MTKBIN_STATE:
+                newState = new MtkBinDecoderState();
+                break;
+            }
+            states.put(stateStr, newState);
+            return newState;
+        }
+    }
+}
---
+++ trunk/BT747/BT747_mdeweerd/src/gps/log/LogFileInfo.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package gps.log;
+
+/**
+ * Object keeping information about a log file.
+ * 
+ * @author Mario De Weerd
+ * 
+ */
+public class LogFileInfo {
+    private String path;
+    private int card; // For some systems
+    private int startTime;
+    private int endTime;
+    private GPSRecord activeFileFields;
+
+    public final GPSRecord getActiveFileFields() {
+        return activeFileFields;
+    }
+
+    public final void setActiveFileFields(final GPSRecord activeFileFields) {
+        this.activeFileFields = activeFileFields;
+    }
+
+    /**
+     * 
+     */
+    public LogFileInfo(final String path, final int startTimeUTC,
+            final int endTimeUTC) {
+        this.path = path;
+        startTime = startTimeUTC;
+        endTime = endTimeUTC;
+    }
+
+    /**
+     * 
+     */
+    public LogFileInfo(final String path, final int card,
+            final int startTimeUTC, final int endTimeUTC) {
+        this(path, startTimeUTC, endTimeUTC);
+        this.card = card;
+    }
+
+    public LogFileInfo(final String path, final int card) {
+        this.path = path;
+        this.card = card;
+    }
+
+    public final String getPath() {
+        return path;
+    }
+
+    public final void setPath(final String path) {
+        this.path = path;
+    }
+
+    public final int getCard() {
+        return card;
+    }
+
+    public final void setCard(final int card) {
+        this.card = card;
+    }
+
+    public final int getStartTime() {
+        return startTime;
+    }
+
+    public final void setStartTime(final int startTime) {
+        this.startTime = startTime;
+    }
+
+    public final int getEndTime() {
+        return endTime;
+    }
+
+    public final void setEndTime(final int endTime) {
+        this.endTime = endTime;
+    }
+
+}
