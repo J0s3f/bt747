09d05f135408984234adf07a686105b1e6d89d1c
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2009-05-10 16:06:10 +0000

    Releasing working copy to trunk

---
+++ trunk/BT747/src/gps/log/GPSFilter.java
@@ -0,0 +1,201 @@
+// ********************************************************************
+// *** BT 747 ***
+// *** April 14, 2007 ***
+// *** (c)2007 Mario De Weerd ***
+// *** m.deweerd@ieee.org ***
+// *** ********************************************************** ***
+// *** Software is provided "AS IS," without a warranty of any ***
+// *** kind. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND WARRANTIES,***
+// *** INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS ***
+// *** FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY ***
+// *** EXCLUDED. THE ENTIRE RISK ARISING OUT OF USING THE SOFTWARE ***
+// *** IS ASSUMED BY THE USER. See the GNU General Public License ***
+// *** for more details. ***
+// *** *********************************************************** ***
+package gps.log;
+
+import bt747.sys.JavaLibBridge;
+
+/**
+ * This class implements a filter for a {@link GPSRecord}.
+ * 
+ * A {@link GPSRecord} can be presented to the doFilter method and it will
+ * indicate whether the record must be kept or not.
+ * 
+ * This class simplifies setting filter options in the application.
+ * 
+ * @author Mario De Weerd
+ */
+public class GPSFilter {
+    private int startTime = 0; // Seconds since 1/1/1970
+    // TODO: could fix potential problem with negative values for date.
+    private int endTime = 0x7FFFFFFF; // Seconds since 1/1/1970
+    private int validMask = 0xFFFFFFFE; // Valid mask
+    private int rcrMask = 0xFFFFFFFF;
+
+    /**
+     * Index to trackpoint filter (in array of GPSFilter).
+     */
+    public static final int TRKPT = 0;
+    /**
+     * Index to waypoint filter (in array of GPSFilter).
+     */
+    public static final int WAYPT = 1;
+
+    /**
+     * Get the first date of the period to include.
+     * 
+     * @return The first date of the period to include (UTC time value).
+     */
+    public final int getStartTime() {
+        return startTime;
+    }
+
+    /**
+     * Set the first date of the period to include (UTC time value).
+     */
+    public final void setStartTime(final int startTime) {
+        this.startTime = startTime;
+    }
+
+    /**
+     * Get the last date of the period to include.
+     * 
+     * @return The last date of the period to include (UTC time value).
+     */
+    public final int getEndTime() {
+        return endTime;
+    }
+
+    /**
+     * Set the last date of the period to include (UTC time value).
+     */
+    public final void setEndTime(final int endTime) {
+        this.endTime = endTime;
+    }
+
+    /**
+     * Sets the 'RCR' filter mask for the given filter type and the currently
+     * active filters.
+     * 
+     * Use the following constants:<br>-
+     * {@link gps.BT747Constants#RCR_TIME_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_SPEED_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_DISTANCE_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_BUTTON_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_APP0_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_APP1_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_APP2_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_APP3_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_APP4_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_APP5_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_APP6_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_APP7_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_APP8_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_APP9_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_APPY_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_APPZ_MASK}<br>-
+     * {@link gps.BT747Constants#RCR_ALL_APP_MASK}
+     * 
+     */
+    public final int getRcrMask() {
+        return rcrMask;
+    }
+
+    /**
+     * Sets the 'RCR' filter mask for the given filter type and the currently
+     * active filters.
+     * 
+     * @param rcrMask
+     *                The filter mask to set for the rcr filter. Use the
+     *                following constants:<br>-
+     *                {@link gps.BT747Constants#RCR_TIME_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_SPEED_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_DISTANCE_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_BUTTON_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_APP0_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_APP1_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_APP2_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_APP3_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_APP4_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_APP5_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_APP6_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_APP7_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_APP8_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_APP9_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_APPY_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_APPZ_MASK}<br>-
+     *                {@link gps.BT747Constants#RCR_ALL_APP_MASK}
+     */
+    public final void setRcrMask(final int rcrMask) {
+        this.rcrMask = rcrMask;
+    }
+
+    /**
+     * Gets the 'Valid' filter mask.
+     * 
+     * Use the following constants:<br>-
+     * {@link gps.BT747Constants#VALID_NO_FIX_MASK} <br>-
+     * {@link gps.BT747Constants#VALID_SPS_MASK} <br>-
+     * {@link gps.BT747Constants#VALID_DGPS_MASK} <br>-
+     * {@link gps.BT747Constants#VALID_PPS_MASK} <br>-
+     * {@link gps.BT747Constants#VALID_RTK_MASK} <br>-
+     * {@link gps.BT747Constants#VALID_FRTK_MASK} <br>-
+     * {@link gps.BT747Constants#VALID_ESTIMATED_MASK} <br>-
+     * {@link gps.BT747Constants#VALID_MANUAL_MASK} <br>-
+     * {@link gps.BT747Constants#VALID_SIMULATOR_MASK}
+     * 
+     */
+    public final int getValidMask() {
+        return validMask;
+    }
+
+    /**
+     * Sets the 'Valid' filter mask.
+     * 
+     * @param validMask
+     *                The filter mask to set for the validity filter. Use the
+     *                following constants:<br>-
+     *                {@link gps.BT747Constants#VALID_NO_FIX_MASK} <br>-
+     *                {@link gps.BT747Constants#VALID_SPS_MASK} <br>-
+     *                {@link gps.BT747Constants#VALID_DGPS_MASK} <br>-
+     *                {@link gps.BT747Constants#VALID_PPS_MASK} <br>-
+     *                {@link gps.BT747Constants#VALID_RTK_MASK} <br>-
+     *                {@link gps.BT747Constants#VALID_FRTK_MASK} <br>-
+     *                {@link gps.BT747Constants#VALID_ESTIMATED_MASK} <br>-
+     *                {@link gps.BT747Constants#VALID_MANUAL_MASK} <br>-
+     *                {@link gps.BT747Constants#VALID_SIMULATOR_MASK}
+     * 
+     */
+    public final void setValidMask(final int validMask) {
+        this.validMask = validMask;
+    }
+
+    /**
+     * Filter the GPS record
+     * 
+     * @param r
+     *                The GPS record
+     * @return true if the record is selected by the filter
+     */
+    public boolean doFilter(final GPSRecord r) {
+        // Filter the record information
+        final boolean result = (((r.utc >= 0) && (r.utc <= 3600 * 24)) || ((r.utc >= startTime) && (r.utc <= endTime)))
+                && (!r.hasValid() || ((r.valid & validMask) != 0))
+                && (!r.hasRcr() || ((r.rcr & rcrMask) != 0));
+        // bt747.sys.Generic.debug(r.toString()+" "+result);
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        return startTime + "<UTC<" + endTime + "\n" + "VALIDMASK="
+                + JavaLibBridge.unsigned2hex(validMask, 8) + "\n" + "RCRMASK="
+                + JavaLibBridge.unsigned2hex(rcrMask, 8) + "\n";
+        // return super.toString();
+    }
+}
---
+++ trunk/BT747/src_j2se/net/sf/bt747/j2se/app/agps/J2SEAGPS.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package net.sf.bt747.j2se.app.agps;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import bt747.sys.Generic;
+
+/**
+ * @author Mario
+ * 
+ */
+public class J2SEAGPS {
+    // Transystem download information:
+    // ftp://tsi0001:xxxxxx@www.transystem.com.tw/
+    // Files: MTK14.EPO and MTK7d.EPO
+
+    private final static String TRANS_FTP_SITE = "http://bt747.free.fr/";
+    private final static String TRANS_AGPS_14d = TRANS_FTP_SITE + "MTK14.EPO";
+    private final static String TRANS_AGPS_7d = TRANS_FTP_SITE + "MTK7d.EPO";
+
+    public static final byte[] getAGPS7d() {
+        return getBytesFromUrl(TRANS_AGPS_7d);
+    }
+
+    public static final byte[] getBytesFromUrl(String urlString) {
+        byte[] result = null;
+        try {
+            final URL url = new URL(urlString);
+            final URLConnection urlc = url.openConnection();
+            urlc.setConnectTimeout(5000);
+            urlc.setReadTimeout(5000);
+            final InputStream ins = urlc.getInputStream(); // To download
+            // OutputStream os = urlc.getOutputStream(); // To upload
+            final ByteArrayOutputStream bout = new ByteArrayOutputStream(120 * 1024);
+            final byte[] buf = new byte[1024];
+            while (true) {
+                final int n = ins.read(buf);
+                if (n == -1) {
+                    break;
+                }
+                bout.write(buf, 0, n);
+            }
+            result = bout.toByteArray();
+            bout.close();
+        } catch (Exception e) {
+            Generic.debug("Problem downloading AGPS data", e);
+        }
+        return result;
+    }
+
+}
