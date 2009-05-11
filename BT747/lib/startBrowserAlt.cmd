570f42e0aa8aa10bb8b245dfafbbed7af203131e
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2009-05-11 21:20:35 +0000

    Simplified release

--- BT747/lib/startBrowserAlt.cmd
+++
@@ -1,4 +0,0 @@
-@echo on
-if "%JAVA_HOME%1"=="1" set JAVA_HOME=D:\java1.6\j2se\build_d
-rem cp ../ndist/jdic.jar ./lib
-%JAVA_HOME%\bin\java -cp "lib\jdic.jar;lib\jdic_native_applet.jar" -jar Browser.jar
