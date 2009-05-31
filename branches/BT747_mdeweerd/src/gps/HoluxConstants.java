2d56470a1ee466928dbbf0c5e64f5016d6d2ce0c
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2009-05-31 17:45:11 +0000

    Merged changes from rtomanek's branch (setting/read of Device name done).

--- branches/BT747_mdeweerd/src/gps/HoluxConstants.java
+++ branches/BT747_mdeweerd/src/gps/HoluxConstants.java
@@ -12,21 +12,43 @@ package gps;
  */
 public class HoluxConstants {
     /**
-     * Prefix commands. Needs to be before many other commands. 
+     * Prefix commands. 
+     * Needs to be before many other commands. 
      * Parameters: none
      */  
     public static final String PHLX_CMD_PREFIX = "PHLX828";
 
     /**
-     * Set device name. Needs to be prefixed with PHLX_CMD_PREFIX
+     * Set device name. 
+     * Needs to be prefixed with PHLX_CMD_PREFIX
      * Parameters:
      * - Device name string
      */  
     public static final String PHLX_NAME_SET_REQUEST = "PHLX830";
+
+    /**
+     * To GPS: Get device name.
+     * Needs to be prefixed with PHLX_CMD_PREFIX
+     * Parameters: none
+     */  
+    public static final String PHLX_NAME_GET_REQUEST = "PHLX831";
 	
-	/**
-     * Erase log command
+    /**
+     * From GPS: Current device name.
+     * Parameters:
+     * - Device name string
+     */  
+    public static final String PHLX_NAME_GET_RESPONSE = "PHLX863";
+
+    /**
+     * To GPS: Erase log command.
      * Parameters: none
      */  
     public static final String PHLX_LOG_ERASE = "PHLX839";
+
+    /**
+     * From GPS: Log erased.
+     * Parameters: none
+     */  
+    public static final String PHLX_LOG_ERASE_ACK = "PHLX870";
 }
