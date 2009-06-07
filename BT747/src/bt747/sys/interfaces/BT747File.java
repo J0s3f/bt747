d77c1f5cc52fca5f39acfe53a3f9272480e4a03e
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2009-06-07 21:33:43 +0000

    Releasing working copy to trunk

---
+++ BT747/src/bt747/sys/interfaces/BT747File.java
@@ -0,0 +1,93 @@
+// ********************************************************************
+// *** BT 747 ***
+// *** April 14, 2007 ***
+// *** (c)2007 Mario De Weerd ***
+// *** m.deweerd@ieee.org ***
+// *** ********************************************************** ***
+// *** Software is provided "AS IS," without a warranty of any ***
+// *** kind. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND WARRANTIES,***
+// *** INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS ***
+// *** FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY ***
+// *** EXCLUDED. THE ENTIRE RISK ARISING OUT OF USING THE SOFTWARE ***
+// *** IS ASSUMED BY THE USER. See the GNU General Public License ***
+// *** for more details. ***
+// *** *********************************************************** ***
+package bt747.sys.interfaces;
+
+/**
+ * Defines the interface needed for the system translation functions.
+ * 
+ * @author Mario De Weerd
+ * 
+ */
+public interface BT747File extends BT747InputStream, BT747OutputStream {
+
+    /**
+     * Get the current file size.
+     * 
+     * @return File size.
+     */
+    int getSize();
+
+    /**
+     * Determine if the file exists.
+     * 
+     * @return true when the file exists.
+     */
+    boolean exists();
+
+    /**
+     * Deletes the file
+     * 
+     * @return true if success.
+     */
+    boolean delete();
+
+    /**
+     * Creates a directory.
+     * 
+     * @return true if success.
+     */
+    boolean createDir();
+
+    /**
+     * Close the file.
+     * 
+     * @return true if success.
+     */
+    boolean close();
+
+    /**
+     * Set the file position.
+     * 
+     * @param pos
+     * @return true if success.
+     */
+    boolean setPos(int pos);
+
+    /**
+     * Determine if the file handle is open.
+     * 
+     * @return true when open.
+     */
+    boolean isOpen();
+
+    /**
+     * Get the file's path.
+     * 
+     * @return File path as a string.
+     */
+    String getPath();
+
+    /**
+     * Error number of the last error that occurred.
+     * 
+     * @return
+     */
+    int getLastError();
+
+    /**
+     * Get modification time (UTC)
+     */
+    int getModificationTime();
+}
---
+++ BT747/src/gps/mvc/ProtectedDevControllerIF.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package gps.mvc;
+
+/**
+ * @author Mario
+ * 
+ */
+interface ProtectedDevControllerIF extends DeviceControllerIF {
+    /**
+     * Request specific data from the device.
+     * 
+     * @param dataType
+     * @return true if supported.
+     */
+    public boolean reqData(final int dataType);
+
+    /**
+     * Called regularly so that the controller can notify the log download
+     * handler for instance.
+     */
+    public void notifyRun();
+    
+    /**
+     * The environment indicates a disconnect happened.
+     */
+    public void notifyDisconnected();
+}
