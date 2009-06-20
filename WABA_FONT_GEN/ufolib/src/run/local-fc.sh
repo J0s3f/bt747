d13eed5504d0281ebd64334242aec362d2b3c2f2
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2009-06-20 20:16:45 +0000

    Branch for mcmatt1

--- WABA_FONT_GEN/ufolib/src/run/local-fc.sh
+++
@@ -1,4 +0,0 @@
-#/usr/local/jdk1.3/jre/bin/java -classpath /home/vera/Sonst/Dict:/home/vera/Sonst/Dict/CUP:/home/vera/Lang/Java/GNUgetopt/java-getopt-1.0.9.jar:/home/vera/Sonst/PalmDev/waba/superwaba-dist/341bfx/superwaba/classes:/home/vera/Sonst/PalmDev/waba/superwaba-dist/341bfx:/usr/home/vera/Sonst/Dict/jdict-cvsd/jdict/ufolib/dest ufolib.convert.Bdfuff x33.bdf -s n -n genXX -l 0 -w 1 -p 17
-
-/usr/local/jdk1.3/jre/bin/java -classpath /home/vera/Lang/Java/XML/bin/xerces-1_4_3/xercesSamples.jar:/home/vera/Lang/Java/XML/bin/xerces-1_4_3/xerces.jar:/home/vera/Sonst/Dict:/home/vera/Sonst/Dict/CUP:/home/vera/Lang/Java/GNUgetopt/java-getopt-1.0.9.jar:/home/vera/Sonst/PalmDev/waba/superwaba-dist/341bfx/superwaba/classes:/home/vera/Sonst/PalmDev/waba/superwaba-dist/341bfx:/usr/home/vera/Sonst/Dict/jdict-cvsd/jdict/ufolib/dest ufolib.fontizer.Controller -c
-
