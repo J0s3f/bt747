3c479dbdffe1518ba09b529c69a7b389f15006b2
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2010-02-15 22:13:33 +0000

    Fixed 'run.j2se' when launched from eclipse + lib updates in start scripts.

--- run_j2se.bat
+++ run_j2se.bat
@@ -6,7 +6,7 @@ set            MYLIB=%MYROOTPATH%lib
 set         RXTXPATH=%MYLIB%\rxtx-2.1-7-bins-r2
 set MYSYSTEMRXTXBINS=%RXTXPATH%\Windows\i368-mingw32
 set             PATH=%MYSYSTEMRXTXBINS%;%MYROOTPATH%;%JAVA_HOME%\bin;%PATH%
-set        CLASSPATH=%MYDIST%\BT747_j2se.jar;%MYLIB%\swingx-ws.jar;%MYLIB%\swingx.jar;%RXTXPATH%\RXTXcomm.jar;%MYDIST%\libBT747.jar;%MYLIB%\swing-layout-1.0.3.jar;%MYLIB%\jopt-simple-2.4.1.jar;%CLASSPATH%
+set        CLASSPATH=%MYDIST%\BT747_j2se.jar;%MYLIB%\swingx-ws.jar;%MYLIB%\swingx.jar;%RXTXPATH%\RXTXcomm.jar;%MYDIST%\libBT747.jar;%MYLIB%\swing-layout-1.0.3.jar;%MYLIB%\jopt-simple-2.4.1.jar;%MYLIB%\jchart2d-3.1.0.jar;%CLASSPATH%
 
 REM java -Dbt747_prefix="COM" -Dbt747_settings="bt747settings.pdb" waba.applet.Applet BT747
 
@@ -16,8 +16,8 @@ rem echo CLASSPATH = %CLASSPATH%
 SET  MEM_HEAP_OPTION=-Xmx192m
 
 REM Uncommenting the next line will enable some logging by RXTX to file 'asdf'.
-#set  DEBUG_OPTION=-Dgnu.io.log.mode=FILE_MODE
-#set CLASSPATH=%RXTXPATH%\RXTXcomm-debug.jar;%CLASSPATH%
+REM set  DEBUG_OPTION=-Dgnu.io.log.mode=FILE_MODE
+REM set CLASSPATH=%RXTXPATH%\RXTXcomm-debug.jar;%CLASSPATH%
 
 REM Change javaw to java in next line to see startup and debug messages
 if x"%*"==x"debug" goto debug:
