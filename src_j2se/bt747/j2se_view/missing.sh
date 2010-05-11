15c2863f2bc742c6d82e17a6ef3471a4a436095d
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2010-05-11 20:19:55 +0000

    Solved conflict of windows sort with unix sort.

--- src_j2se/bt747/j2se_view/missing.sh
+++ src_j2se/bt747/j2se_view/missing.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+SORT=/usr/bin/sort
 dos2unix -U Bundle.properties
 dos2unix -U Bundle_$1.properties
-perl -p -e 's/^(.*?)=.*/$1=/g' Bundle.properties | sort -u > ${TEMP}/en.lst
-perl -p -e 's/^(.*?)\s*=.*/$1=/g' Bundle_$1.properties | sort -u > ${TEMP}/$1.lst
+perl -p -e 's/^(.*?)=.*/$1=/g' Bundle.properties | ${SORT} -u > ${TEMP}/en.lst
+perl -p -e 's/^(.*?)\s*=.*/$1=/g' Bundle_$1.properties | ${SORT} -u > ${TEMP}/$1.lst
 diff ${TEMP}/en.lst ${TEMP}/$1.lst | perl -n -e 'if(s/^<\s+(.*)/$1/) { print; }' > ${TEMP}/$1_missing.lst
 grep -F -f ${TEMP}/$1_missing.lst Bundle.properties > $1_missing.lst
 cat $1_missing.lst
