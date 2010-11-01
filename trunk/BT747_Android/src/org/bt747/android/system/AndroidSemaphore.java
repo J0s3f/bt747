09201209c305c6c6cb3f99788263c4a45718d817
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2010-11-01 12:47:45 +0000

    Created BT747_common directory and copied system files.

---
+++ trunk/BT747_Android/src/org/bt747/android/system/AndroidSemaphore.java
@@ -0,0 +1,41 @@
+package org.bt747.android.system;
+
+import java.util.concurrent.Semaphore;
+
+import bt747.sys.interfaces.BT747Semaphore;
+
+// ********************************************************************
+// *** BT 747 ***
+// *** April 14, 2007 ***
+// *** (c)2007 Mario De Weerd ***
+// *** m.deweerd@ieee.org ***
+// *** ********************************************************** ***
+// *** Software is provided "AS IS," without a warranty of any ***
+// *** kind. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND WARRANTIES,***
+// *** INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS ***
+// *** FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY ***
+// *** EXCLUDED. THE ENTIRE RISK ARISING OUT OF USING THE SOFTWARE ***
+// *** IS ASSUMED BY THE USER. See the GNU General Public License ***
+// *** for more details. ***
+// *** *********************************************************** ***
+/**
+ * @author Mario De Weerd
+ */
+public final class AndroidSemaphore implements BT747Semaphore {
+    private Semaphore available;
+
+    public AndroidSemaphore(final int value) {
+        available = new Semaphore(value, true);
+    }
+
+    public void down() {
+        try {
+            available.acquire();
+        } catch (final InterruptedException e) {
+        }
+    }
+
+    public void up() {
+        available.release();
+    }
+}
