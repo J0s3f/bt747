b25fe176da7fd99acab3b72c707f72ac9be6748b
lanthale <lanthale@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2011-01-25 20:13:04 +0000

    - Uncomment code to be compatible with old versions

--- trunk/swingx-ws/src/beaninfo/org/jdesktop/swingx/JXMapKitBeanInfo.java
+++ trunk/swingx-ws/src/beaninfo/org/jdesktop/swingx/JXMapKitBeanInfo.java
@@ -6,20 +6,22 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package org.jdesktop.swingx;
 
 //import org.jdesktop.swingx.editors.EnumPropertyEditor;
+
+import org.jdesktop.swingx.editors.EnumPropertyEditor;
 
 /**
  *
  * @author joshy
  */
 public class JXMapKitBeanInfo extends BeanInfoSupport {
-    
+
     public JXMapKitBeanInfo() {
         super(JXMapKit.class);
     }
+
     protected void initialize() {
         //setPropertyEditor(DefaultProvidersPropertyEditor.class, "defaultProvider");
         setPreferred(true, "defaultProvider");
@@ -28,9 +30,11 @@ public class JXMapKitBeanInfo extends BeanInfoSupport {
         setPreferred(true, "zoomButtonsVisible");
         setPreferred(true, "zoomSliderVisible");
     }
-//    public static final class DefaultProvidersPropertyEditor extends EnumPropertyEditor {
-//        public DefaultProvidersPropertyEditor() {
-//            super(JXMapKit.DefaultProviders.class);
-//        }
-//    }
+
+    public static final class DefaultProvidersPropertyEditor extends EnumPropertyEditor {
+
+        public DefaultProvidersPropertyEditor() {
+            super(JXMapKit.DefaultProviders.class);
+        }
+    }
 }
