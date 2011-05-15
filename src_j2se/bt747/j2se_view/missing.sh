eeb4027db8ab5cb8ab4fc1aa4e7a48a7503c5853
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2011-05-15 11:38:47 +0000

    'dos2unix' arguments changed - removed forcing dos to unix.

--- src_j2se/bt747/j2se_view/missing.sh
+++ src_j2se/bt747/j2se_view/missing.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 SORT=/usr/bin/sort
 # Bundle.properties
-dos2unix -U Bundle.properties
-dos2unix -U Bundle_$1.properties
+dos2unix Bundle.properties
+dos2unix Bundle_$1.properties
 perl -p -e 's/^(.*?)=.*/$1=/g' Bundle.properties | ${SORT} -u > ${TEMP}/en.lst
 perl -p -e 's/^(.*?)\s*=.*/$1=/g' Bundle_$1.properties | ${SORT} -u > ${TEMP}/$1.lst
 diff ${TEMP}/en.lst ${TEMP}/$1.lst | perl -n -e 'if(s/^<\s+(.*)/$1/) { print; }' > ${TEMP}/$1_missing.lst
 grep -F -f ${TEMP}/$1_missing.lst Bundle.properties > $1_missing.lst
 
 # BT747base
-dos2unix -U ../../net/sf/bt747/j2se/app/resources/BT747base.properties
-dos2unix -U ../../net/sf/bt747/j2se/app/resources/BT747base_$1.properties
+dos2unix ../../net/sf/bt747/j2se/app/resources/BT747base.properties
+dos2unix ../../net/sf/bt747/j2se/app/resources/BT747base_$1.properties
 perl -p -e 's/^(.*?)=.*/$1=/g' ../../net/sf/bt747/j2se/app/resources/BT747base.properties | ${SORT} -u > ${TEMP}/en.lst
 perl -p -e 's/^(.*?)\s*=.*/$1=/g' ../../net/sf/bt747/j2se/app/resources/BT747base_$1.properties | ${SORT} -u > ${TEMP}/$1.lst
 diff ${TEMP}/en.lst ${TEMP}/$1.lst | perl -n -e 'if(s/^<\s+(.*)/$1/) { print; }' > ${TEMP}/$1_missing.lst
