11831a53157cc25d4568be96a35ff4126e191023
mdeweerd <mdeweerd@729ccadd-3348-0410-aaf9-990fd8a5ef07>
2011-12-24 01:37:34 +0000

    Added, updated translations.

--- src_j2se/bt747/j2se_view/all_missing.sh
+++ src_j2se/bt747/j2se_view/all_missing.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -vx
-LANGS="ca de es fr it nl pl pt ru zh ro"
+LANGS="ca de es fa fr it nl pl pt ro ru zh"
 for i in $LANGS ; do
 ./missing.sh $i
 ../../../../uploadBT747.bat ${i}_missing.lst missing/${i}_missing.lst
